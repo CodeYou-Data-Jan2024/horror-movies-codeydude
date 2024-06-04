8c826f7ee8e4f06d617965a0b01477c8da80a9bf
Lavin Lalchandani <lavinl27@gmail.com>
2024-06-04 18:42:07 -0400

    update complete

--- sql/horror_movies.sql
+++ sql/horror_movies.sql
@@ -1,5 +1,8 @@
 -- Add your SQL here
-select id,name,imdb_rating from movies
+select id as Movie_ID, 
+name as Movie_Title, 
+imdb_rating as Rating 
+from movies
 where year <= "1985"
 and genre = "horror"
 order by imdb_rating desc
